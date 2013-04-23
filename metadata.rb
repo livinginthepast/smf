521e68626bdecd571f4c7c33849e34e2de77d850
Eric Saxby <sax@livinginthepast.org>
2013-04-23 16:36:32 -0700

    Bump version to 0.6.9

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@modcloth.com'
 license          'Apache 2.0'
 description      'A light weight resource provider (LWRP) for SMF (Service Management Facility)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.6.8'
+version          '0.6.9'
 
 supports 'smartos'
 
