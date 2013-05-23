3073c515e377d3ac3f87e7ccecd29654f273e1df
Jon DeCamp <jon.decamp@nordstrom.com>
2013-05-23 09:39:07 -0700

    Initial Commit

---
+++ metadata.rb
@@ -0,0 +1,5 @@
+name             'COOKBOOK_NAME'
+license          'All rights reserved'
+description      'Installs/Configures COOKBOOK_NAME'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.0'
