c3d1ca36907960f93bef7c70319e8ad70da266e6
Eric Saxby <pair+sax@wanelo.com>
2013-05-23 11:34:40 -0700

    Depend on fixed rbac

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'ops@modcloth.com'
 license          'Apache 2.0'
 description      'A light weight resource provider (LWRP) for SMF (Service Management Facility)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 supports 'smartos'
 
-depends 'rbac', '>= 1.0.0'
+depends 'rbac', '>= 1.0.1'
 
 recommends 'resource-control'
