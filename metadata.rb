1c14af638d4f0202bd59d5905af068b21f4e14c0
Eric Saxby <sax@livinginthepast.org>
2014-08-20 16:53:22 -0700

    2.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sax@livinginthepast.org'
 license          'MIT'
 description      'A light weight resource provider (LWRP) for SMF (Service Management Facility)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.6'
+version          '2.1.0'
 
 supports 'smartos'
 
