c9c9b4d22c8592fe433fe9559fd081be9d4d2539
markgibbons <mark.gibbons@nordstrom.com>
2014-12-04 17:53:27 -0800

    UIS-822: Fix tailor complaints.

--- test/integration/cookbooks/test_smf/recipes/smf_solaris_testing.rb
+++ test/integration/cookbooks/test_smf/recipes/smf_solaris_testing.rb
@@ -1,9 +1,9 @@
 # Update system files for later test kitchen setup processing
 bash 'Kitchen_setup_changes' do
-code <<-EOH
+  code <<-EOH
   echo 'Defaults env_keep = "PATH"' >> /etc/sudoers
   echo 'PATH=/usr/xpg4/bin:/usr/bin:' >> /etc/default/login
   echo 'SUPATH=/usr/xpg4/bin:/usr/sbin:/usr/bin:' >> /etc/default/login
-EOH
-not_if 'grep xpg4 /etc/sudoers'
+  EOH
+  not_if 'grep xpg4 /etc/sudoers'
 end
